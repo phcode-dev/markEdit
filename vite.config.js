be6621ffcd5695119ec2beb250476549e2845d9f
abose <arun@core.ai>
2025-01-20 11:19:59 +0530

    rollup options

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
         outDir: '../dist/',
         emptyOutDir: true,
         cssCodeSplit: false, // Ensure all CSS is combined into a single file
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/index.js', // Single JavaScript file
+                assetFileNames: 'assets/styles.css', // Single CSS file
+            },
+        },
     },
 });
