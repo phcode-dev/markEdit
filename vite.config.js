dad116c75e9b054cc1886eac8289f442ebe58207
abose <arun@core.ai>
2025-01-20 11:17:40 +0530

    refactor project paths

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: './', // Use relative paths
+    root: 'src',
     optimizeDeps: {
         include: ['@milkdown/plugin-listener', '@milkdown/crepe'], // Ensure Milkdown plugins are pre-bundled
     },
     build: {
+        outDir: '../dist/',
+        emptyOutDir: true,
         cssCodeSplit: false, // Ensure all CSS is combined into a single file
     },
 });
