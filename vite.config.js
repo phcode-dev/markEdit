bfd71528188bed32f29f1bd5c21b22cf1f947875
abose <arun@core.ai>
2025-01-20 11:11:06 +0530

    min css

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ export default defineConfig({
     optimizeDeps: {
         include: ['@milkdown/plugin-listener', '@milkdown/crepe'], // Ensure Milkdown plugins are pre-bundled
     },
+    build: {
+        cssCodeSplit: false, // Ensure all CSS is combined into a single file
+    },
 });
