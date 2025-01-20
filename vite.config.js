940510ea6491bd2920a79c82300619c28bcb88c8
abose <arun@core.ai>
2025-01-20 10:54:30 +0530

    initial

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: './', // Use relative paths
+    optimizeDeps: {
+        include: ['@milkdown/plugin-listener', '@milkdown/crepe'], // Ensure Milkdown plugins are pre-bundled
+    },
+});
